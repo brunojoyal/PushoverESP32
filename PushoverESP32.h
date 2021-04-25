f1cb3b044c824fc891c4a6be77c344b98abba50a
Bruno Joyal <brunojoyal@gmail.com>
2021-04-25 09:45:47 -0400

    First commit

---
+++ PushoverESP32.h
@@ -0,0 +1,39 @@
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+class Pushover
+{
+private:
+	String _token="";
+	String _user="";
+	String _message="";
+	String _device="";
+	String _title="";
+	String _url="";
+	String _url_title="";
+	int8_t _priority = -1;
+	uint32_t _timestamp = 0;
+	boolean _html;
+	uint16_t _timeout = 5000;
+	String _sound;
+
+public:
+	Pushover(String, String);
+	void setToken(String);
+	void setUser(String);
+	void setMessage(String);
+	void setDevice(String);
+	void setTitle(String);
+	void setUrl(String);
+	void setUrlTitle(String);
+	void setPriority(int8_t);
+	void setRetry(uint16_t);
+	void setExpire(uint16_t);
+	void setTimestamp(uint32_t);
+	void setSound(String);
+	void setHTML(boolean);
+	void setTimeout(uint16_t);
+	int send(void);
+};
